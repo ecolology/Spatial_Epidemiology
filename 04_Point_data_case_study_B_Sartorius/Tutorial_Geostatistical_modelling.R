257e502c6c9ea9ec51d8965ed3f611cdd00a1b80
Tatianaproboste <38739705+Tatianaproboste@users.noreply.github.com>
2023-11-21 08:17:39 +1000

    Add files via upload

--- 04_Point_data_case_study_B_Sartorius/Tutorial_Geostatistical_modelling.R
+++ 04_Point_data_case_study_B_Sartorius/Tutorial_Geostatistical_modelling.R
@@ -2,11 +2,13 @@
 
 #This contains files that allows to reproduce the exploratory analysis, 
 #parameter estimation and spatial prediction for the geostatistical model 
-#containing all the covariates presented in the paper "Model building and 
-#assessment of the impact of covariates for disease prevalence mapping in 
-#low-resource settings: to explain and to predict."
+#containing all the covariates presented in the paper "Giorgi E, Fronterrè C, 
+#Macharia PM, Alegana VA, Snow RW, Diggle PJ. Model building and assessment of 
+#the impact of covariates for disease prevalence mapping in low-resource 
+#settings: to explain and to predict. Journal of The Royal Society Interface. 
+#2021 Jun 2;18(179):20210104."
 
-#Source: https://github.com/giorgilancs/covariates/
+#Tutorial source: https://github.com/giorgilancs/covariates/
 
 #"TZ_2015.csv" is the file containing the data. Each row corresponds to a sampled community with total number of tested people ("Ex"), nuber of positive cases ("Pf") and geographical locations. The column "utm_x" is the x-coordinate and "utm_y" is the y-coordinate in UTM; longitude and latitude are also reported in "Long" and "Lat", respectively. The name of the covariates in the data correspond to the same names and abbreviation used in the main manuscript.
 
@@ -20,9 +22,10 @@ library(mgcv)
 library(ggplot2)
 library(splines)
 
-setwd("C:/Users/uqbsarto/Downloads/")
+setwd("C:/Users/uqbsarto/OneDrive - The University of Queensland/Documents/Benn/Teaching/Geostatistical modelling/")
 
-##Part 1: exploring relationships between covariates and prevalence; testing for residual spatial correlation.
+##Part 1: exploring relationships between covariates and prevalence; 
+##testing for residual spatial correlation.
 
 tz <- read.csv("TZ_2015.csv")
 
@@ -34,6 +37,25 @@ tz$logit <- log((tz$Pf+0.5)/(tz$Ex-tz$Pf+0.5))
 tz$log.Population <- log(tz$Population)
 tz$log.Precipitation <- log(tz$Precipitation)
 
+head(tz)
+
+ggplot(tz, aes(x=Long, y=Lat))+ 
+  geom_point()+  
+  theme_bw()
+
+library(leaflet)
+library(viridis)
+
+pal <- colorBin("viridis", bins = c(0, 0.25, 0.5, 0.75, 1))
+leaflet(tz) %>%
+  addProviderTiles(providers$CartoDB.Positron) %>%
+  addCircles(lng = ~Long, lat = ~Lat, color = ~ pal(logit)) %>%
+  addLegend("bottomright",
+    pal = pal, values = ~logit,
+    title = "Logit."
+  ) %>%
+  addScaleBar(position = c("bottomleft"))
+
 # Temperature
 plot.temp <- ggplot(tz, aes(x = Temperature, 
                             y = logit)) + geom_point() +
@@ -41,8 +63,6 @@ plot.temp <- ggplot(tz, aes(x = Temperature,
   stat_smooth(method = "gam", formula = y ~ s(x),se=FALSE)+
   stat_smooth(method = "lm", formula = y ~ x,
               col="green",lty="dashed",se=FALSE)
-
-plot.temp
 
 # EVI
 plot.evi <- ggplot(tz, aes(x = EVI, 
@@ -69,7 +89,6 @@ plot.pop <- ggplot(tz, aes(x = log.Population,
   stat_smooth(method = "gam", formula = y ~ s(x),se=FALSE)+
   stat_smooth(method = "lm", formula = y ~ x ,
               col="green",lty="dashed",se=FALSE)
-
 
 # Precipitation
 plot.prec <- ggplot(tz, aes(x = log.Precipitation, 
@@ -103,6 +122,12 @@ grid.arrange(plot.temp,plot.evi,
 
 # Testing for residual spatial correlation
 # using the variogram
+spat.corr.diagnostic(Pf ~ 1,
+                     units.m = ~ Ex,
+                     coords = ~utm_x+utm_y,
+                     likelihood = "Binomial",
+                     data=tz)
+
 spat.corr.diagnostic(Pf ~ Temperature+
                        EVI+I((EVI-0.36)*(EVI-0.35))+
                        NTL+I((NTL-9)*(NTL>9))+
@@ -131,14 +156,10 @@ tz$log.Precipitation <- log(tz$Precipitation)
 glm.fit <- 
   glm(cbind(Pf,Ex-Pf) ~
         Temperature+
-        
         EVI+
         I((EVI-0.35)*(EVI>0.35))+
-        
         NTL + 
-        
         log(Population)+
-        
         log(Precipitation),
       family=binomial,data=tz)
 
@@ -159,21 +180,17 @@ done <- FALSE
 # Monte Calo maximum likelihood estimation
 
 # Control parameters of the Monte Carlo algorithm
-control.mcmc <- control.mcmc.MCML(n.sim=110000,burnin=10000,
+control.mcmc <- control.mcmc.MCML(n.sim=20000,burnin=10000,
                                   thin=10)
 
 # Fitting of the model
 fit.MCML <- 
   binomial.logistic.MCML(Pf ~
                            Temperature+
-                           
                            EVI+
                            I((EVI-0.35)*(EVI>0.35))+
-                           
                            NTL + 
-                           
                            log(Population)+
-                           
                            log(Precipitation),
                          par0=par0,
                          start.cov.pars = c(par0[p+2],par0[p+3]/par0[p+1]),
@@ -182,6 +199,14 @@ fit.MCML <-
                          control.mcmc = control.mcmc,
                          kappa=0.5,method="nlminb",
                          data=tz)
+
+variog.diagnostic.glgm(
+fit.MCML,
+n.sim = 200,
+uvec = NULL,
+plot.results = TRUE,
+which.test = "both"
+)
 
 summary(fit.MCML)
 
